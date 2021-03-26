0517f984f1ef74780734c28d3b00a5fc557bb370
Mohannad Bakbouk <MOhannadBakbouk@gmail.com>
2021-03-27 00:01:05 +0300

    Initial Commit

---
+++ DynamicCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicCell
+//
+//  Created by Mohannad on 27.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
